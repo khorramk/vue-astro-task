a3128cc7861a49713f3e95de13ea2447d5e1c5b4
Khorram Khondkar <khorramk.kbsk@gmail.com>
2024-01-11 11:47:59 +0000

    Minor chnages for build fix

--- src/components/AppCta.vue
+++ src/components/AppCta.vue
@@ -4,15 +4,14 @@
   <header class="cta-hero" role="banner" aria-label="Hero Section">
     <div class="cta-img-container">
         <img
-          src="/img/ctaMobilePlaceholder.svg"
+          src="./img/ctaPlaceholder.svg"
           width="600"
           height="1000"
           role="img"
           loading="eager"
           srcset="
-            /img/ctaMobilePlaceholder.svg  600w,
-            /img/ctaPlaceholder.svg       1200w
-          "
+            ./img/mobilePlaceholder.svg 600w,
+            ./img/ctaPlaceholder.svg 1200w"
           sizes="(max-width: 700px) 400px, (max-width: 800px) 1400px"
         />
     </div>
